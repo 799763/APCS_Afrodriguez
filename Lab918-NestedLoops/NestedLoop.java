b4efa4f001a07be8daf9454e5ddb8fd2477a4069
799763 <54371477+799763@users.noreply.github.com>
2019-09-18 10:21:32 -0700

    Add files via upload

---
+++ Lab918-NestedLoops/NestedLoop.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class NestedLoop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NestedLoop
+{
+    int x;
+    public static void ProblemA(){
+        for (int x=1; x<0; x++){
+            for (int y=0; y<0; y++){
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ProblemB(){
+
+    }
+}
+
