6e55614949f6263b0446f45410514f32a9712dfb
799763 <54371477+799763@users.noreply.github.com>
2019-09-20 10:29:33 -0700

    Add files via upload

---
+++ Lab920-SentenceBuilder/RunnerClass.java
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ *
+ * @author (Fernando Rodriguez)
+ * @version (920)
+ */
+public class RunnerClass
+{
+    public static void main(){
+     SentenceBuilder sb = new SentenceBuilder(); 
+    }
+}
---
+++ Lab920-SentenceBuilder/SentenceBuilder.java
@@ -0,0 +1,61 @@
+
+/**
+ *
+ *
+ * @author (Fernando Rodriguez)
+ * @version (920)
+ */
+public class SentenceBuilder
+{
+    // Nouns: puppy, pizza, snake, doorbell, girl, building  (Things)
+    String[] nouns = {"rock", "puppy", "cat", "sock", "ship", "hero", "monkey", "baby", "match"};
+    // Verbs:  jumps, runs, flies, eats, makes, wants, sings, skips (Actions)
+    String[] verbs = {"bid", "began", "bent","caught", "chose", "came", "ate", "drank", "fell"};
+    // Adjectives: little, silly, funny, stuffy, shady, hopeful, graceful (modifies nouns)
+    String[] adjetives = {"happy", "funny", "hopeful", "mad", "sad", "little", "shady", "wierd", "stuffy" };    
+    // Determiners: the, my, your, his, hers, our, their, Fred's (clarify noun phrase)
+    String[] determiners = {"my", "your", "Bob's", "their", "his", "her", "hers", "our", "Hanna's" };
+    // Adverbs: quickly, happily, smoothly, often, very, never (modifies verbs)
+    String[] Adverbs = {"slowly", "quickly", "never", "often", "eagerly", "early", "earnestly", "easily", "calmly" };
+    // Prepositions: over, near, on top of, behind, next to, inside of, under (position)
+    String[] Prepositions = {"along", "within", "across", "under", "over", "infront of","outside of", "away from", "inside of"  };
+    public SentenceBuilder(){
+
+    }
+    public void loadSentences(){
+    
+    }
+    public String getSentence(){
+      String s = "";
+      return s;
+    }
+    public String getNounPhrase(){
+      String np = determiner[] + adjetive[] + noun[];
+      return np;
+    }
+    public String getPrepositionalPhrase(){
+      String gp = "";
+      return gp;
+    }
+    public String getVerbPhrase(){
+    
+    }
+    public String getRandomNoun(){
+        
+    }
+    public String getRandomVerb(){
+    
+    }
+    public String getRandomAdjective(){
+    
+    }
+    public String getRandomAdverb(){
+    
+    }
+    public String getRandomDeterminer(){
+    
+    }
+    public String getRandomPreposition(){
+    
+    }
+    }
