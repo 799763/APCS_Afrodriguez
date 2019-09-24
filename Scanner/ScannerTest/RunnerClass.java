2961957f813473850da95bc727ecd60777286b16
799763 <54371477+799763@users.noreply.github.com>
2019-09-24 10:30:46 -0700

    Add files via upload

---
+++ Scanner/ScannerTest/RunnerClass.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class RunnerClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RunnerClass
+{
+    public static void main(){
+     RunnerClass so = new RunnerClass();
+     RunnerClass po = new RunnerClass();
+     
+    }
+}
