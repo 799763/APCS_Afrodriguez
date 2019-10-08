fa042cfe8d86155e1c1efc9fcaa91137a3d55b5c
Fernando Rodriguez <frodriguez@WS-ACADBUS.EDU>
2019-10-08 10:12:59 -0700

    This Porbably Ruined My Parse Code

--- Scanner/ScannerTest/Runner.java
+++ Scanner/ScannerTest/Runner.java
@@ -13,13 +13,13 @@ public class Runner{
     public static void main(){
         Scanner kb = new Scanner(System.in);
         String inputStr = "";
-        System.out.println("Enter a name or type \"quit\" to quit");
-        inputStr = kb.nextLine();
-        System.out.println(inputStr);
+        
+        
         while(!inputStr.equals("quit")){
             Person p = new Person();
         }
-        
-        
+        System.out.println(inputStr);
+        System.out.println("Enter a name or type \"quit\" to quit");
+        inputStr = kb.nextLine();
     }
 }
