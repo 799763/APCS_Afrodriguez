84c4e3534d6beed1c765c2e015517119c306b048
Fernando Rodriguez <frodriguez@WS-ACADBUS.EDU>
2019-10-25 09:28:27 -0700

    Student lab

---
+++ ProjectStudentList/StudList.java
@@ -0,0 +1,30 @@
+import java.util.*;
+/**
+ * Write a description of class StudList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StudList
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    ArrayList<Student> studList = new ArrayList<Student>();
+    /**
+     * Constructor for objects of class StudList
+     */
+    public StudList()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     */
+    public void addStudent(String fullName)
+    {
+        
+        
+    }
+}
---
+++ ProjectStudentList/StudListRunner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+/**
+ * Write a description of class StudentListRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StudListRunner
+{
+    StudList StudList = new StudList();
+}
