05413ed7dea99370c8c71b20fb137ddeeb0eb627
Fernando Rodriguez <frodriguez@WS-ACADBUS.EDU>
2019-10-31 08:30:34 -0700

    1031 StringCheck

---
+++ 1031_StringCheck/StringCheck.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.Scanner;
+/**
+ * Write a description of class StringCheck here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StringCheck
+{
+    // instance variables
+    String[] y = {"x"};// delete after done (just here to get rid of error sign)
+    ArrayList words = new ArrayList();
+    private int x;
+    /**
+     * Constructor for objects of class StringCheck
+     */
+    public StringCheck()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    public static void main(){
+     StringCheck string = new StringCheck();
+    }
+    public ArrayList<String> wordChecker(String[] str, String Key){
+      words.add(str); 
+      return words;
+    }
+    public String[] wordChecker(ArrayList<String> str, String StringKey){
+      
+        
+      return y;// replace y with actual return later
+    }
+}
