09a1be1dcd99d3855d7f5b05efad2daa672877f8
Fernando Rodriguez <frodriguez@WS-ACADBUS.EDU>
2019-11-04 10:16:12 -0800

    1104 String Check

--- 1031_StringCheck/StringCheck.java
+++ 1031_StringCheck/StringCheck.java
@@ -1,48 +1,65 @@
 import java.util.*;
 /**
- * Write a description of class StringCheck here.
- *
  * @author (Fernando Rodriguez)
  * @version (31 October 2019)
  */
 public class StringCheck
 {
     // instance variables    
-    private ArrayList<String> words = new ArrayList<String>();
-    private String[] wordList = new String[10];
-    private String Key;
+    private ArrayList<String> wordList = new ArrayList<String>();
+    private String[] wordArray = {"Cat", "Catcher", "Catchup", "CatDog", "Dog", "Doggy", "DogCat"};
     /**
      * Constructor for objects of class StringCheck
      */
     public StringCheck()
     {
         // initialise instance variables
-        String Key = "";
+        String Key = "Cat";
+        loadList();       
+        ArrayList<String> keyList = wordChecker1(wordArray, Key); 
+        printList(keyList);
     }
-
     public static void main(){
         StringCheck string = new StringCheck();
     }
-
-    public ArrayList<String> wordChecker(String[] str, String Key){
-        // This method should return an ArrayList
+    public void loadList(){
+      wordList.add("Cat");
+      wordList.add("Cather");
+      wordList.add("Cathup");
+      wordList.add("CatDog");
+      wordList.add("Dog");
+      wordList.add("Doggy");
+      wordList.add("DogCat");      
+    }
+    public void printList(ArrayList<String> list){
+      for(int i = 0; i < wordList.size(); i++){  
+        System.out.println(wordList.get(i));
+      }
+    }
+    /*
+     * This method should take in an Array and return an ArrayList
+     */
+    public ArrayList<String> wordChecker1(String[] str, String k){
         ArrayList<String> tempList = new ArrayList<String>();
-        for(int i= 0 ; i <str.length; i++){
-          if(Key.indexOf(Key) > -1){
-            for(int q = 0; q > str.length(); q++){
-              String z = str[q];
-              if(z.indexOf(Key) > -1){
-                tempList.add(z);
-              }
-            }
+        for(int i = 0 ; i < str.length; i++){
+          if(str[i].indexOf(k) > -1){
+             tempList.add(str[i]);
           }
         }
         return tempList;
     }
-
-    public String[] wordChecker(ArrayList<String> str, String StringKey){
-        // This method should return an Array
-        
-        return wordList;
-    }
+     // /*
+      // * This method should take in an ArrayList and return an Array
+      // */
+     // public String[] wordChecker2(ArrayList<String> str, String StringKey){
+         // ArrayList<String> tempList = new ArrayList<String>();
+         // for(int i = 0 ; i < str.size(); i++){
+           // if(Key.indexOf(Key) > -1){          
+               // if(str.get(1).indexOf(Key) > -1){
+                 // tempList.add(i);
+               // }
+           // }
+         // }
+         // return wordList;
+     // }
 }
